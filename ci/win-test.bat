169c7d1d9cd1f101a5095c74f0757bd0691cf568
cfalcon <cfalcon@cells.es>
2017-07-13 08:37:11 +0200

    Test ci

---
+++ ci/win-test.bat
@@ -0,0 +1,21 @@
+:: Install (py)tango + deps
+conda install  -y  -c tango-controls pytango=9.2.1
+:: Install itango
+conda install -c tango-controls itango=0.1.6
+:: Loging root env
+activate
+:: Install taurus
+pip install taurus
+:: Install the generated sardana wheel package to test it
+:: Make sure it does not come from cache or pypi
+:: At this point all install_requires dependencies MUST be installed
+:: as this is installing only from dist/
+pip install --pre --find-links dist/ --no-cache-dir --no-index sardana
+
+:: Print Python info
+pip list
+
+:: Test import
+python -c "import sardana"
+:: TODO run testsuite
+::sardanatestsuite
